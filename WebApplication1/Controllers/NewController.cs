b578320d98a1a42bc7d7c7baece98a79e49abc1a
Demo <gianghi060@gmail.com>
2020-05-10 10:27:21 +0700

    init project

---
+++ WebApplication1/Controllers/NewController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class NewController : Controller
+    {
+        public string chuoi() 
+        {
+            return ("hello");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public int so()
+        {
+            return new Random().Next(1000, 10000);
+        }
+    }
+}
